0312956ac10a2f1f37c2dc5f668baa00f2f9607d
Ahad-Abdul-Shaik <96719051+Ahad-Abdul-Shaik@users.noreply.github.com>
2022-01-13 15:53:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+status1="";
+objects=[];
+song="";
+function preload() {
+ song=loadSound('Alarm.mp3')
+}
+function setup() {
+    canvas=createCanvas(380,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    objectDetector= ml5.objectDetector('cocossd',modalLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+function draw() {
+    image(video,0,0,380,380);
+  if(status1 != "") {
+      r=random(255);
+      g=random(255);
+      b=random(255);
+      objectDetector.detect(video,gotResults);
+     for(i=0;i<objects.length;i++){
+        document.getElementById("status").innerHTML = "Status : Objects Detected";
+        document.getElementById("number_of_objects").innerHTML="Number Of Objects "+objects.length;
+        fill(r,g,b);
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+ " " +percent+"%",objects[i].x+15,objects[i].y+15);
+        noFill();
+        stroke(r,g,b);
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+        if (objects[i].label=="person") {
+            document.getElementById("number_of_objects").innerHTML="Baby Found";
+            song.stop();
+        } else {
+            document.getElementById("number_of_objects").innerHTML="Baby Not Found";
+            song.play();
+        }
+     }
+   if(objects.length==0) {
+    document.getElementById("number_of_objects").innerHTML="Baby Not Found";
+    song.play();
+   }
+  }
+}
+function modalLoaded() {
+    console.log("Modal is Loaded");
+    status1= true;
+}
+function gotResults(error,results) {
+   if(error) {
+       console.log(error);
+   }
+   console.log(results);
+   objects=results;
+}
